80d67283c863f37ae4c19d39db4c7477bdf8ee55
Joel de Almeida Souza <joel.almeida@consigamais.com.br>
2019-07-29 19:59:34 -0300

    Init Project

---
+++ desafio-joel/desafio-joel/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  desafio-joel
+//
+//  Created by Joel de Almeida Souza on 29/07/19.
+//  Copyright © 2019 Joel de Almeida Souza. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ desafio-joel/desafio-joelTests/desafio_joelTests.swift
@@ -0,0 +1,34 @@
+//
+//  desafio_joelTests.swift
+//  desafio-joelTests
+//
+//  Created by Joel de Almeida Souza on 29/07/19.
+//  Copyright © 2019 Joel de Almeida Souza. All rights reserved.
+//
+
+import XCTest
+@testable import desafio_joel
+
+class desafio_joelTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
